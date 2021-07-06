a1d11514ad3267799c6a006d16c003d4a5b96966
CHaerem <chris.haerem@gmail.com>
2021-07-06 23:19:37 +0200

    Data handler + new table

--- scraper.py
+++ scraper.py
@@ -9,6 +9,10 @@ from webdriver_manager.chrome import ChromeDriverManager
 import random
 import requests
 import time
+from datetime import datetime
+import re
+import sqlite3
+
 '''
 Setup a chromium engine headless browser
 '''
@@ -26,10 +30,19 @@ def save_page(soup):
     with open("newsweb.html", "w") as file:
         file.write(str(soup))
 
+def insert_data(conn, entry):
+    sql = ''' INSERT INTO dividend_history(ticker,ex_date,dividends_paid,message)
+        VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, entry)
+    conn.commit()
+
+conn = sqlite3.connect('financial-database.db')
 
 driver = setup_browser()
 
-url = "https://newsweb.oslobors.no/search?category=1101&issuer=&fromDate=2011-01-01&toDate=2012-06-01&market=XOSL&messageTitle="
+url = "https://newsweb.oslobors.no/search?category=1101&issuer=&fromDate=2012-01-01&toDate=2013-01-01&market=XOSL&messageTitle="
+
 driver.get(url)
 time.sleep(5)
 soup = BeautifulSoup(driver.page_source, 'lxml')
@@ -47,4 +60,17 @@ table_rows = table.find_all('tr')
 for tr in table_rows:
     td = tr.find_all('td')
     row = [i.text for i in td]
-    print(row)
+    if len(row) == 7:
+        message = row[3].lower()
+        if 'utbytte' in message and ('nok' in message or 'kr' in message):
+            ex_date = datetime.strptime(row[0], '%d.%m.%Y %H:%M')
+            ticker = row[2]
+
+            dividends_paid = re.findall("\d+\.\d+", message) 
+            if len(dividends_paid) == 0:
+                dividends_paid = re.findall("\d+\,\d+", message)
+            
+            if not ex_date or not ticker or len(dividends_paid) > 0: 
+                dividends_paid = dividends_paid[0].replace(',', '.')
+                entry = (ticker, ex_date, dividends_paid, message)
+                insert_data(conn, entry)
